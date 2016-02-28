a715c65afcb52f2a284c676ec8a3c7ba9a982141
shubhsin <luk4shubhs@gmail.com>
2016-02-28 16:22:29 +0530

    Initial Commit

---
+++ AppReviewTimes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AppReviewTimes
+//
+//  Created by Shubham Sorte on 28/02/16.
+//  Copyright © 2016 Shubham Sorte. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
